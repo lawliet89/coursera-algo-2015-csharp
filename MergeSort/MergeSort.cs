09c14ee312c53f79251e153946a577a74cc2acfe
Yong Wen Chua <lawliet89@users.noreply.github.com>
2015-10-06 23:32:11 +0800

    Basic MergeSort

---
+++ MergeSort/MergeSort.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MergeSort
+{
+    public static class MergeSort
+    {
+        public static IEnumerable<T> Sort<T>(IEnumerable<T> numbers)
+            where T: IComparable<T>
+        {
+            var list = numbers as IList<T> ?? numbers.ToList();
+            var size = list.Count();
+
+            if (size < 2)
+                return list;
+
+            var left = list.Take(size/2);
+            var right = list.Skip(size/2);
+
+            var sortedLeft = Sort(left);
+            var sortedRight = Sort(right);
+            return Merge(sortedLeft, sortedRight);
+        }
+
+        private static IEnumerable<T> Merge<T>(IEnumerable<T> left, IEnumerable<T> right)
+            where T : IComparable<T>
+        {
+            var leftList = left as IList<T> ?? left.ToList();
+            var rightList = right as IList<T> ?? right.ToList();
+            var result = new List<T>(leftList.Count() + rightList.Count());
+
+            using (IEnumerator<T> leftIterator = leftList.GetEnumerator(), rightIterator = rightList.GetEnumerator())
+            {
+                var leftHasNext = leftIterator.MoveNext();
+                var rightHasNext = rightIterator.MoveNext();
+                while (leftHasNext || rightHasNext)
+                {
+                    if (!leftHasNext)
+                    {
+                        result.Add(rightIterator.Current);
+                        rightHasNext = rightIterator.MoveNext();
+                    }
+                    else if (!rightHasNext)
+                    {
+                        result.Add(leftIterator.Current);
+                        leftHasNext = leftIterator.MoveNext();
+                    }
+                    // i.e. left < right
+                    else if (leftIterator.Current.CompareTo(rightIterator.Current) <= 0)
+                    {
+                        result.Add(leftIterator.Current);
+                        leftHasNext = leftIterator.MoveNext();
+                    }
+                    else
+                    {
+                        result.Add(rightIterator.Current);
+                        rightHasNext = rightIterator.MoveNext();
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ MergeSort/MergeSortTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MergeSort
+{
+    [TestFixture]
+    public class MergeSortTest
+    {
+        [Test]
+        public void SortBaseCase()
+        {
+            CollectionAssert.IsOrdered(MergeSort.Sort(new[] { 1 }));
+        }
+
+        [Test]
+        public void SortCorrectly()
+        {
+            CollectionAssert.IsOrdered(MergeSort.Sort(new[] { 3, 4, 2, 1, 7, 5, 8, 9, 0, 6}));
+        }
+    }
+}
