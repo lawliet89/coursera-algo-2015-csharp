73aeab61c1141f6ff40a56dc575c68666159ea7b
Yong Wen Chua <lawliet89@users.noreply.github.com>
2015-10-06 22:20:56 +0800

    First Karatsuba implementation

---
+++ Karatsuba/Karatsuba.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Numerics;
+
+namespace Karatsuba
+{
+    // Let's work in Base 10 for now
+    public static class Karatsuba
+    {
+        public static int Base = 10;
+
+        // x = 10^n/2 a + b
+        // y = 10^n/2 c + d
+        // x.y = 10^n ac + 10^(n/2)(ad + bc) + bd
+        public static BigInteger Multiply(BigInteger x, BigInteger y)
+        {
+            if (x < 10 || y < 10)
+            {
+                return x*y;
+            }
+            var n = Math.Max(GetBasePower(x), GetBasePower(y));
+            var halfN = n/2;
+
+            var split = Split(x, halfN);
+            var a = split[0];
+            var b = split[1];
+
+            split = Split(y, halfN);
+            var c = split[0];
+            var d = split[1];
+
+            var ac = Multiply(a, c);
+            var bd = Multiply(b, d);
+
+            var middle = Multiply(a + b, c + d) - ac - bd;
+
+            return BigInteger.Pow(10, halfN * 2)*ac + BigInteger.Pow(10, halfN)*middle + bd;
+        }
+
+        public static int GetBasePower(BigInteger x)
+        {
+            return Convert.ToInt32(Math.Floor(BigInteger.Log10(x)) + 1);
+        }
+
+        // x = 10^n/2 a + b
+        // Returns [a, b]
+        public static BigInteger[] Split(BigInteger x, int halfN)
+        {
+            BigInteger remainder;
+            var quotient = BigInteger.DivRem(x, BigInteger.Pow(Base, halfN), out remainder);
+            return new[] { quotient, remainder };
+        }
+    }
+}
---
+++ Karatsuba/KaratsubaTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Karatsuba
+{
+    [TestFixture]
+    public class KaratsubaTest
+    {
+        [Test]
+        public void MultiplySingleDigitsCorrectly()
+        {
+            Assert.AreEqual(new BigInteger(0), Karatsuba.Multiply(0, 9));
+            Assert.AreEqual(new BigInteger(15), Karatsuba.Multiply(5, 3));
+        }
+
+        [Test]
+        public void GetBasePowerReturnsCorrectly()
+        {
+            Assert.AreEqual(2, Karatsuba.GetBasePower(10));
+            Assert.AreEqual(8, Karatsuba.GetBasePower(10000000));
+
+            Assert.AreEqual(10, Karatsuba.GetBasePower(1257896541));
+        }
+
+        [Test]
+        public void SplitsCorrectly()
+        {
+            Assert.AreEqual(new BigInteger[] { 12, 34}, Karatsuba.Split(1234, 2));
+            Assert.AreEqual(new BigInteger[] { 12, 0 }, Karatsuba.Split(1200, 2));
+            Assert.AreEqual(new BigInteger[] { 100, 0 }, Karatsuba.Split(100000, 3));
+            Assert.AreEqual(new BigInteger[] { 12578, 96541 }, Karatsuba.Split(1257896541, 5));
+
+            Assert.AreEqual(new BigInteger[] { 12, 345 }, Karatsuba.Split(12345, 3));
+            Assert.AreEqual(new BigInteger[] { 10, 0 }, Karatsuba.Split(10000, 3));
+        }
+
+
+        [Test]
+        public void MultiplesCorrectly()
+        {
+            Assert.AreEqual(new BigInteger(2500), Karatsuba.Multiply(50, 50));
+            Assert.AreEqual(new BigInteger(7006652), Karatsuba.Multiply(1234, 5678));
+            Assert.AreEqual(new BigInteger(35936459), Karatsuba.Multiply(4589, 7831));
+            Assert.AreEqual(new BigInteger(83810205), Karatsuba.Multiply(12345, 6789));
+            Assert.AreEqual(new BigInteger(250000000000), Karatsuba.Multiply(500000, 500000));
+        }
+    }
+}
