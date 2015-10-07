c294b3bfc5d62d03c4bb209ee09e092141f8a35e
Yong Wen Chua <lawliet89@users.noreply.github.com>
2015-10-07 22:37:29 +0800

    Add split inversion counting to merge step of MergeSort

--- MergeSort/MergeSort.cs
+++ MergeSort/MergeSort.cs
@@ -10,7 +10,7 @@ namespace MergeSort
             where T: IComparable<T>
         {
             var list = numbers as IList<T> ?? numbers.ToList();
-            var size = list.Count();
+            var size = list.Count;
 
             if (size < 2)
                 return list;
@@ -23,39 +23,54 @@ namespace MergeSort
             return Merge(sortedLeft, sortedRight);
         }
 
-        private static IEnumerable<T> Merge<T>(IEnumerable<T> left, IEnumerable<T> right)
+        public static IEnumerable<T> Merge<T>(IEnumerable<T> left, IEnumerable<T> right)
             where T : IComparable<T>
         {
+            int _; // We are throwing this away
+            return Merge(left, right, out _);
+        }
+
+        public static IEnumerable<T> Merge<T>(IEnumerable<T> left, IEnumerable<T> right, out int splitInversionCount)
+            where T : IComparable<T>
+        {
+            splitInversionCount = 0;
+
             var leftList = left as IList<T> ?? left.ToList();
             var rightList = right as IList<T> ?? right.ToList();
-            var result = new List<T>(leftList.Count() + rightList.Count());
+            var leftCount = leftList.Count;
+            var result = new List<T>(leftCount + rightList.Count);
 
             using (IEnumerator<T> leftIterator = leftList.GetEnumerator(), rightIterator = rightList.GetEnumerator())
             {
                 var leftHasNext = leftIterator.MoveNext();
                 var rightHasNext = rightIterator.MoveNext();
-                while (leftHasNext || rightHasNext)
+                var leftIndex = 0;
+                while (leftHasNext && rightHasNext)
                 {
-                    if (!leftHasNext)
-                    {
-                        result.Add(rightIterator.Current);
-                        rightHasNext = rightIterator.MoveNext();
-                    }
-                    else if (!rightHasNext)
-                    {
-                        result.Add(leftIterator.Current);
-                        leftHasNext = leftIterator.MoveNext();
-                    }
                     // i.e. left <= right
-                    else if (leftIterator.Current.CompareTo(rightIterator.Current) <= 0)
+                    if (leftIterator.Current.CompareTo(rightIterator.Current) <= 0)
                     {
                         result.Add(leftIterator.Current);
                         leftHasNext = leftIterator.MoveNext();
+                        leftIndex++;
                     }
-                    else
+                    else // i.e. left > right and we have split inversion
                     {
+                        splitInversionCount += (leftCount - leftIndex);
                         result.Add(rightIterator.Current);
                         rightHasNext = rightIterator.MoveNext();
+                    }
+                }
+
+                if (leftHasNext || rightHasNext)
+                {
+                    var hasNext = true;
+                    var iterator = leftHasNext ? leftIterator : rightIterator;
+
+                    while (hasNext)
+                    {
+                        result.Add(iterator.Current);
+                        hasNext = iterator.MoveNext();
                     }
                 }
             }
--- MergeSort/MergeSortTest.cs
+++ MergeSort/MergeSortTest.cs
@@ -21,5 +21,18 @@ namespace MergeSort
         {
             CollectionAssert.IsOrdered(MergeSort.Sort(new[] { 3, 4, 2, 1, 7, 5, 8, 9, 0, 6}));
         }
+
+        [Test]
+        public void MergesAndCountsSplitInversionsCorrectly()
+        {
+            var left = new[] {1, 3, 5};
+            var right = new[] {2, 4, 6};
+
+            int splitInversionCount;
+            var merged = MergeSort.Merge(left, right, out splitInversionCount);
+
+            CollectionAssert.IsOrdered(merged);
+            Assert.AreEqual(3, splitInversionCount);
+        }
     }
 }
