8da052bf16894c6a176f0860728bdd57bf8911cc
Yong Wen Chua <lawliet89@users.noreply.github.com>
2015-10-30 19:59:15 +0800

    Implement QuickSort

---
+++ QuickSort/QuickSort.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using Utilities;
+
+namespace QuickSort
+{
+    public static class QuickSort
+    {
+        private enum PivotChoice
+        {
+            First, Last, Median, Random
+        }
+
+        public static IList<T> SortPivotFirst<T>(IList<T> list)
+            where T : IComparable<T>
+        {
+            return Sort(list, PivotChoice.First, 0, list.Count);
+        }
+
+        public static IList<T> SortPivotLast<T>(IList<T> list)
+            where T : IComparable<T>
+        {
+            return Sort(list, PivotChoice.Last, 0, list.Count);
+        }
+
+        public static IList<T> SortPivotMedian<T>(IList<T> list)
+            where T : IComparable<T>
+        {
+            return Sort(list, PivotChoice.Median, 0, list.Count);
+        }
+
+        public static IList<T> SortPivotRandom<T>(IList<T> list)
+            where T : IComparable<T>
+        {
+            return Sort(list, PivotChoice.Random, 0, list.Count);
+        }
+
+        private static IList<T> Sort<T>(IList<T> list, PivotChoice pivotChoice, int firstIndex, int lastIndex)
+            where T : IComparable<T>
+        {
+            var length = firstIndex - lastIndex;
+            if (length < 2)
+                return list;
+
+            int pivotIndex = firstIndex;
+            switch(pivotChoice)
+            {
+                case PivotChoice.Last:
+                    pivotIndex = lastIndex - 1;
+                    break;
+                case PivotChoice.Median:
+                    pivotIndex = firstIndex + Convert.ToInt32(Math.Round(length / 2.0));
+                    break;
+                case PivotChoice.Random:
+                    // Unimplemented
+                    break;
+            }
+
+            if (pivotIndex != firstIndex)
+            {
+                list = list.Swap(pivotIndex, firstIndex);
+            }
+
+            var pivot = list[pivotIndex];
+            var i = 1;
+            for (var j = 1; j < length; ++j)
+            {
+                if (list[firstIndex + j].CompareTo(pivot) < 1)
+                {
+                    list.Swap(firstIndex + j, firstIndex + i);
+                    ++i;
+                }
+            }
+
+            list.Swap(firstIndex, firstIndex + i - 1);
+            Sort(list, pivotChoice, firstIndex, firstIndex + i - 1);
+            Sort(list, pivotChoice, firstIndex + i, lastIndex);
+            
+            return list;
+        }
+    }
+}
---
+++ Utilities/List.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Utilities
+{
+    public static class List
+    {
+        public static IList<T> Swap<T>(this IList<T> list, int indexA, int indexB)
+        {
+            T tmp = list[indexA];
+            list[indexA] = list[indexB];
+            list[indexB] = tmp;
+            return list;
+        }
+    }
+}
