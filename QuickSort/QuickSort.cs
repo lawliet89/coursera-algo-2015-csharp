cec192f9e2e70401233574e4fc3b383dcbba18d5
Yong Wen Chua <lawliet89@users.noreply.github.com>
2015-10-30 21:13:42 +0800

    Add test and fix QuickSort

--- QuickSort/QuickSort.cs
+++ QuickSort/QuickSort.cs
@@ -11,45 +11,46 @@ namespace QuickSort
             First, Last, Median, Random
         }
 
-        public static IList<T> SortPivotFirst<T>(IList<T> list)
+        public static long SortPivotFirst<T>(IList<T> list)
             where T : IComparable<T>
         {
             return Sort(list, PivotChoice.First, 0, list.Count);
         }
 
-        public static IList<T> SortPivotLast<T>(IList<T> list)
+        public static long SortPivotLast<T>(IList<T> list)
             where T : IComparable<T>
         {
             return Sort(list, PivotChoice.Last, 0, list.Count);
         }
 
-        public static IList<T> SortPivotMedian<T>(IList<T> list)
+        public static long SortPivotMedian<T>(IList<T> list)
             where T : IComparable<T>
         {
             return Sort(list, PivotChoice.Median, 0, list.Count);
         }
 
-        public static IList<T> SortPivotRandom<T>(IList<T> list)
+        public static long SortPivotRandom<T>(IList<T> list)
             where T : IComparable<T>
         {
             return Sort(list, PivotChoice.Random, 0, list.Count);
         }
 
-        private static IList<T> Sort<T>(IList<T> list, PivotChoice pivotChoice, int firstIndex, int lastIndex)
+        // Returns the number of comparisons
+        private static long Sort<T>(IList<T> list, PivotChoice pivotChoice, int firstIndex, int lastIndex)
             where T : IComparable<T>
         {
-            var length = firstIndex - lastIndex;
+            var length = lastIndex - firstIndex;
             if (length < 2)
-                return list;
+                return 0;
 
-            int pivotIndex = firstIndex;
-            switch(pivotChoice)
+            var pivotIndex = firstIndex;
+            switch (pivotChoice)
             {
                 case PivotChoice.Last:
                     pivotIndex = lastIndex - 1;
                     break;
                 case PivotChoice.Median:
-                    pivotIndex = firstIndex + Convert.ToInt32(Math.Round(length / 2.0));
+                    pivotIndex = firstIndex + Convert.ToInt32(Math.Round(length/2.0));
                     break;
                 case PivotChoice.Random:
                     // Unimplemented
@@ -61,7 +62,7 @@ namespace QuickSort
                 list = list.Swap(pivotIndex, firstIndex);
             }
 
-            var pivot = list[pivotIndex];
+            var pivot = list[firstIndex];
             var i = 1;
             for (var j = 1; j < length; ++j)
             {
@@ -73,10 +74,11 @@ namespace QuickSort
             }
 
             list.Swap(firstIndex, firstIndex + i - 1);
-            Sort(list, pivotChoice, firstIndex, firstIndex + i - 1);
-            Sort(list, pivotChoice, firstIndex + i, lastIndex);
-            
-            return list;
+            var count = (long) length - 1;
+            count += Sort(list, pivotChoice, firstIndex, firstIndex + i - 1);
+            count += Sort(list, pivotChoice, firstIndex + i, lastIndex);
+
+            return count;
         }
     }
 }
---
+++ QuickSort/QuickSortTest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace QuickSort
+{
+    [TestFixture]
+    public class QuickSortTest
+    {
+        public static IList<int>[] Lists =
+        {
+            new []{3, 4, 2, 1, 7, 5, 8, 9, 0, 6},
+            new []{ 5, 2, 10, 8, 1, 9, 4, 3, 6, 7 }
+        };
+
+        [Test, TestCaseSource(nameof(Lists))]
+        public void SortByFirstPivot(IList<int> list)
+        {
+            var actualList = new List<int>(list);
+            QuickSort.SortPivotFirst(actualList);
+            CollectionAssert.IsOrdered(actualList);
+        }
+
+        [Test, TestCaseSource(nameof(Lists))]
+        public void SortByLastPivot(IList<int> list)
+        {
+            var actualList = new List<int>(list);
+            QuickSort.SortPivotLast(actualList);
+            CollectionAssert.IsOrdered(actualList);
+        }
+
+        [Test, TestCaseSource(nameof(Lists))]
+        public void SortByMedianPivot(IList<int> list)
+        {
+            var actualList = new List<int>(list);
+            QuickSort.SortPivotMedian(actualList);
+            CollectionAssert.IsOrdered(actualList);
+        }
+    }
+}
